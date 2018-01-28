737e82c32f4904629c452ff6c41b2936df5a36c6
Zack <zngrizzle@gmail.com>
2018-01-28 11:03:48 -0800

    k

--- BeamMeUp/Assets/Scripts/Hensen.cs
+++ BeamMeUp/Assets/Scripts/Hensen.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class Hensen : MonoBehaviour {
+public class Hensen : MonoBehaviour
+{
     public float speedDamp = .1f;
-    public Transform Exit;
+    Transform exit;
     int speedHashParam;
-    NavMeshAgent agent;
+    [HideInInspector]
+    public NavMeshAgent agent;
     Animator anim;
-
-	// Use this for initialization
-	void Start () {
+    bool isExiting;
+    // Use this for initialization
+    void Start()
+    {
         anim = GetComponent<Animator>();
         speedHashParam = Animator.StringToHash("Speed");
         agent = GetComponent<NavMeshAgent>();
-        agent.isStopped = true;    
+        agent.isStopped = true;
+        exit = GameObject.Find("Exit").transform;
+
         GoToExit();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         var speed = agent.desiredVelocity.magnitude;
         anim.SetFloat(speedHashParam, speed, speedDamp, Time.deltaTime);
-	}
 
-    void GoToExit()
+
+        if (isExiting)
+        {
+            if (agent.remainingDistance < 6)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void GoToExit()
     {
-        agent.SetDestination(Exit.position);
+        agent.SetDestination(exit.position);
         agent.isStopped = false;
-      
+        isExiting = true;
     }
 }
--- BeamMeUp/Assets/Scripts/TeleportPad.cs
+++ BeamMeUp/Assets/Scripts/TeleportPad.cs
@@ -3,16 +3,121 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleportPad : MonoBehaviour {
-    public ParticleSystem[] vfxTeleporters;
+    public Transform[] teleporterPos;
+    public GameObject vfxTeleport;
+    public GameObject vfxTeleportShockWave;
+    public GameObject[] hensenPrefabs; // listed worst to best
 
+    public float teleportDuration;
+    public float hensenPause = 1f;
 
-    public void StartTeleportVFX(int teleporter)
+    void Start()
     {
-        vfxTeleporters[teleporter].Play();
+        BadTeleport();
     }
 
-    public void StopTeleportVFX(int teleporter)
+
+    //public void StartTeleportVFX(int teleporter)
+    //{
+    //    vfxTeleporters[teleporter].Play();
+    //}
+
+    //public void StopTeleportVFX(int teleporter)
+    //{
+    //    vfxTeleporters[teleporter].Stop();
+    //}
+
+    public void Teleport(float value)
     {
-        vfxTeleporters[teleporter].Stop();
+        if (value <= 20)
+        {
+            BadTeleport();
+        }
+        else if(value <= 40)
+        {
+            KindaBadTeleport();
+        }
+        else if (value <= 60)
+        {
+            DecentTeleport();
+        }
+        else if (value <= 80)
+        {
+            OkayTeleport();
+        }
+        else if (value <= 90)
+        {
+            GoodTeleport();
+        }else
+        {
+            GreatTeleport();
+        }
+       
+    }
+
+    void BadTeleport()
+    {
+        //play bad music
+        var trans = teleporterPos[Random.Range(0, 3)];
+        var partSys = Instantiate(vfxTeleport).GetComponent<ParticleSystem>();
+        partSys.transform.position = trans.position;
+        var main = partSys.main;
+
+        StartCoroutine(Teleport(partSys, hensenPrefabs[0], teleportDuration, trans));
+        partSys.Play();
+        
+    }
+
+    void KindaBadTeleport()
+    {
+
+    }
+
+    void DecentJob()
+    {
+
+    }
+
+    void DecentTeleport()
+    {
+
+    }
+
+    void OkayTeleport()
+    {
+
+    }
+
+    void GoodTeleport()
+    {
+
+    }
+
+    void GreatTeleport()
+    {
+
+    }
+
+    IEnumerator Teleport(ParticleSystem teleportPartSys, GameObject hensenPrefab, float timeToInvoke, Transform teleporterTrans)
+    {
+        yield return new WaitForSeconds(timeToInvoke);
+
+        var hensenGO = Instantiate(hensenPrefab);
+        hensenGO.transform.position = teleporterTrans.position;
+        hensenGO.transform.rotation = teleporterTrans.rotation;
+
+        var teleportShock = Instantiate(vfxTeleportShockWave);
+        teleportShock.transform.position = teleportPartSys.transform.position;
+
+        Destroy(teleportShock, teleportShock.GetComponent<ParticleSystem>().main.duration);
+        Destroy(teleportPartSys.gameObject);
+
+     
+        yield return new WaitForSeconds(hensenPause);
+        var hensen = hensenGO.GetComponent<Hensen>();
+        hensen.GoToExit();
+
+
+
     }
 }
